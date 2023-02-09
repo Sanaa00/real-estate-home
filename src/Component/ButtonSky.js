cf752726cb30e5fbae4764d9d51085da8de1e77f
Sana Rzgar <sanarizgar34@gmail.com>
2023-02-09 15:10:39 +0300

    create : sky blue button

---
+++ src/Component/ButtonSky.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ButtonSky({ props }) {
+  return (
+    <button className="px-4 py-1 text-sm bg-blue-100 rounded-tl-2xl text-blue-800 ml-4">
+      {props}
+    </button>
+  );
+}
+
+export default ButtonSky;

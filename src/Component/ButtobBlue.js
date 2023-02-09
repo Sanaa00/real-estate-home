f4cb5ec43ce7cbd72c95c88e87b22eeeaeb5d27d
Sana Rzgar <sanarizgar34@gmail.com>
2023-02-09 15:10:04 +0300

    create: blue  button component

---
+++ src/Component/ButtobBlue.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ButtobBlue({ props }) {
+  return (
+    <button className="px-4 py-1 bg-blue-500 text-sm rounded-tl-2xl text-white ">
+      {props}
+    </button>
+  );
+}
+
+export default ButtobBlue;

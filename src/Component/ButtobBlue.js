a773c1cc8b7413e48aecb4eede2e5399bd2dacfb
Sana Rzgar <sanarizgar34@gmail.com>
2023-02-11 20:30:52 +0300

    update

--- src/Component/ButtobBlue.js
+++ src/Component/ButtobBlue.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-function ButtobBlue({ props }) {
+function ButtobBlue({ text, width }) {
   return (
-    <button className="px-4 py-1 bg-blue-500 text-sm rounded-tl-2xl text-white ">
-      {props}
+    <button
+      className={`px-4 py-1 bg-blue-500 text-sm rounded-tl-2xl text-white  w-${width}`}
+    >
+      {text}
     </button>
   );
 }
--- src/Component/ButtonSky.js
+++ src/Component/ButtonSky.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-function ButtonSky({ props }) {
+function ButtonSky({ text, width, onClick }) {
   return (
-    <button className="px-4 py-1 text-sm bg-blue-100 rounded-tl-2xl text-blue-800 ml-4">
-      {props}
+    <button
+      className={`px-4 py-1 text-sm bg-blue-100 rounded-tl-2xl text-blue-800 ml-4 w-40`}
+      onClick={() => onClick}
+    >
+      {text}
     </button>
   );
 }

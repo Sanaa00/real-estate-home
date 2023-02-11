c684be438cb9fa43d0cba12140061d10f9e71f86
Sana Rzgar <sanarizgar34@gmail.com>
2023-02-11 20:46:49 +0300

    update:footer

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,12 +1,16 @@
 import React from "react";
 import logo from "../images/logo.png";
 import ButtobBlue from "./ButtobBlue";
+import linkedin from "../images/linkedin.png";
+import behance from "../images/behance.png";
+import facebook from "../images/facebook.png";
+import twitter from "../images/twitter.png";
 function Footer() {
   const icons = [
-    { id: 1, icon: "A" },
-    { id: 2, icon: "B" },
-    { id: 3, icon: "C" },
-    { id: 4, icon: "D" },
+    { id: 1, icon: linkedin },
+    { id: 2, icon: behance },
+    { id: 3, icon: facebook },
+    { id: 4, icon: twitter },
   ];
   const Company = [
     { id: 1, text: "Why Findhome?" },
@@ -26,53 +30,61 @@ function Footer() {
     { id: 3, text: "example@gmail.com", icon: "" },
   ];
   return (
-    <div className="bg-gray-100 pt-20 flex justify-between text-sm p-6">
-      <div className="flex flex-col justify-between">
-        <img src={logo} alt="logo" className="h-10 w-10" />
-        <p className="mt-3">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        </p>
-        <div className="mt-3">
-          <input
-            placeholder="Type Your Email "
-            className="w-full rounded-tl-xl py-1 px-2 relative overflow-hidden"
-          />
-          <div className=" absolute right-0 bottom-0">
-            <ButtobBlue text="Send" />
+    <div className="bg-gray-100 pt-20 flex flex-col justify-between  text-sm p-6">
+      <div className="flex justify-between ">
+        <div className="flex flex-col justify-between">
+          <img src={logo} alt="logo" className="h-10 w-10" />
+          <p className="mt-3">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+          <div className="mt-3 overflow-hidden relative">
+            <input
+              placeholder="Type Your Email "
+              className="w-full rounded-tl-xl py-1 px-2 relative "
+            />
+            <div className=" absolute right-0 bottom-0">
+              <ButtobBlue text="Send" />
+            </div>
+          </div>
+          <div className="flex mt-3">
+            {icons.map((icon) => {
+              return (
+                <img
+                  src={icon.icon}
+                  alt="social"
+                  key={icon.id}
+                  className="rounded-full h-10 w-10 bg-blue-500 flex justify-center items-center mr-2"
+                />
+              );
+            })}
           </div>
         </div>
-        <div className="flex mt-3">
-          {icons.map((icon) => {
+        <div className="flex flex-col justify-between">
+          <p className="font-bold">Company</p>
+          {Company.map((text) => {
+            return <p key={text.id}>{text.text}</p>;
+          })}
+        </div>
+        <div className="flex flex-col justify-between">
+          <p className="font-bold">Support</p>
+          {support.map((supp) => {
+            return <p key={supp.id}>{supp.text}</p>;
+          })}
+        </div>
+        <div className="flex flex-col justify-between">
+          <p className="font-bold">Contact Us</p>
+          {Countacts.map((con) => {
             return (
-              <div
-                key={icon.id}
-                className="rounded-full h-10 w-10 bg-blue-500 flex justify-center items-center mr-2"
-              >
-                {icon.icon}
+              <div key={con.id} className="flex">
+                <p>{con.text}</p>
               </div>
             );
           })}
         </div>
       </div>
-      <div className="flex flex-col justify-between">
-        {Company.map((text) => {
-          return <p key={text.id}>{text.text}</p>;
-        })}
-      </div>
-      <div className="flex flex-col justify-between">
-        {support.map((supp) => {
-          return <p key={supp.id}>{supp.text}</p>;
-        })}
-      </div>
-      <div className="flex flex-col justify-between">
-        {Countacts.map((con) => {
-          return (
-            <div key={con.id} className="flex">
-              <p>{con.text}</p>
-            </div>
-          );
-        })}
-      </div>
+      <p className="text-blue-400 text-center mt-5">
+        Copyright - All Right Reserved
+      </p>
     </div>
   );
 }

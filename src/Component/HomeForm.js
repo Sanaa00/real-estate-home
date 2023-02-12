9f3d1b45e3c73d9e73175e1d3b1246be019b4c49
Sana Rzgar <sanarizgar34@gmail.com>
2023-02-12 21:12:50 +0300

    update:add selectors

--- src/Component/HomeForm.js
+++ src/Component/HomeForm.js
@@ -1,46 +1,53 @@
 import React from "react";
 import ButtobBlue from "./ButtobBlue";
+import Selector from "./Selector";
 
 function HomeForm() {
   const homeForm = [
     {
+      id: 1,
       type: "text",
-
       title: "Enter your keywords",
       placeholder: "Keywords",
       img: "",
     },
     {
+      id: 2,
       type: "selector",
       title: "All state",
       placeholder: "States",
       img: "",
     },
     {
+      id: 3,
       type: "selector",
       title: "All Cities",
       placeholder: "City",
       img: "",
     },
     {
+      id: 4,
       type: "selector",
       title: "All Offers",
       placeholder: "Offer",
       img: "",
     },
     {
+      id: 5,
       type: "selector",
       title: "All Listing Types",
       placeholder: "Listing",
       img: "",
     },
     {
+      id: 6,
       type: "selector",
       title: "All Bedrooms",
       placeholder: "Bedrooms",
       img: "",
     },
     {
+      id: 7,
       type: "selector",
       title: "  ",
       placeholder: "Price Range",
@@ -53,7 +60,7 @@ function HomeForm() {
       <div className="grid  grid-cols-4 grid-rows-2 justify-between items-end gap-5 ">
         {homeForm.map((form) => {
           return (
-            <div>
+            <div key={form.id}>
               <p className="text-sm font-semibold">{form.title}</p>
               {form?.type === "text" ? (
                 <input
@@ -61,12 +68,12 @@ function HomeForm() {
                   className="bg-gray-100 rounded-tl-xl px-4 py-1 text-sm mt-2 w-full"
                 />
               ) : (
-                "Selector zyad bka"
+                <Selector />
               )}
             </div>
           );
         })}
-        <ButtobBlue props={"Search Properites"} />
+        <ButtobBlue text={"Search Properites"} />
       </div>
     </div>
   );

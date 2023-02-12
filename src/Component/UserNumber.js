dc23f8113935b103f859b76ccb6950d526bc34a5
Sana Rzgar <sanarizgar34@gmail.com>
2023-02-12 21:15:16 +0300

    updated

--- src/Component/UserNumber.js
+++ src/Component/UserNumber.js
@@ -2,21 +2,15 @@ import React from "react";
 
 function UserNumber() {
   const users = [
-    { num: "2k+", text: "Property sold" },
-    {
-      num: "5+",
-      text: "experience",
-    },
-    {
-      num: "7",
-      text: "Awards",
-    },
+    { id: 1, num: "2k+", text: "Property sold" },
+    { id: 2, num: "5+", text: "experience" },
+    { id: 3, num: "7", text: "Awards" },
   ];
   return (
     <div className="flex my-5 justify-between">
       {users.map((user) => {
         return (
-          <div className="flex-col flex items-center ">
+          <div key={user.id} className="flex-col flex items-center ">
             <p className="text-xl font-bold">{user.num}</p>
             <p className="text-sm">{user.text}</p>
           </div>

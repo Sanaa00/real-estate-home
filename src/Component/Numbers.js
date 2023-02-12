4413fd9db359b8d9d8f0f3c2adc04dfcf4e4b119
Sana Rzgar <sanarizgar34@gmail.com>
2023-02-12 21:13:51 +0300

    update:numbers

--- src/Component/Numbers.js
+++ src/Component/Numbers.js
@@ -2,26 +2,20 @@ import React from "react";
 
 function Numbers() {
   const numbers = [
-    { num: "15000+", title: "Successful Trades" },
-    {
-      num: "6200+",
-      title: "Happy Customers",
-    },
-    {
-      num: "360+",
-      title: "Awards Won",
-    },
-    {
-      num: "17+",
-      title: "Years Of Experience",
-    },
+    { id: 1, num: "15000+", title: "Successful Trades" },
+    { id: 2, num: "6200+", title: "Happy Customers" },
+    { id: 3, num: "360+", title: "Awards Won" },
+    { id: 4, num: "17+", title: "Years Of Experience" },
   ];
   return (
     <>
       <div className="bg-white p-6 flex">
         {numbers.map((number) => {
           return (
-            <div className="flex flex-col justify-between items-center w-full">
+            <div
+              key={number.id}
+              className="flex flex-col justify-between items-center w-full"
+            >
               <p className="text-xl font-bold">{number.num}</p>
               <p className="text-sm">{number.title}</p>
             </div>

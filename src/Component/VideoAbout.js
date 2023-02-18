5c5978f629634604b2a7fb0e949651411cc073d8
Sana Rzgar <sanarizgar34@gmail.com>
2023-02-18 20:07:19 +0300

    add video for about section

---
+++ src/Component/VideoAbout.js
@@ -0,0 +1,16 @@
+import React from "react";
+import YouTube from "react-youtube";
+function VideoAbout() {
+  const videoId = "5hemeiTVIG0";
+  const style = {
+    height: "290",
+    width: "540",
+  };
+  return (
+    <div>
+      <YouTube videoId={videoId} opts={style} />
+    </div>
+  );
+}
+
+export default VideoAbout;
